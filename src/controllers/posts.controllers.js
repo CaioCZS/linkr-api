c96d9041129467d9fe37d80e7ad7944bedbc9599
Caio <caiozs21@gmail.com>
2023-05-31 14:50:05 -0300

    feat: like/dislike and get hashtags

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,20 +1,46 @@
-import { createPostDB, getPostByPostUrlAndUserId } from "../repository/posts.repositories.js";
+import {
+  createPostDB,
+  dbDislikePost,
+  dbLikePost,
+  getPostByPostUrlAndUserId,
+} from "../repository/posts.repositories.js"
 
-export async function createPost(req, res){
-    const {id} = req.params;
-    const {description, postUrl} = req.body;
+export async function createPost(req, res) {
+  const { id } = req.params
+  const { description, postUrl } = req.body
 
-    try{
-        await createPostDB(description, postUrl, id);
-        const post = await getPostByPostUrlAndUserId (postUrl, id);
-        const result = {
-            id: post.rows[0].id,
-            postUrl: post.rows[0].postUrl,
-            userId: post.rows[0].userId
-        }
-        return res.status(201).send(result);
-
-    } catch(err){
-        return res.status(500).send(err.message);
+  try {
+    await createPostDB(description, postUrl, id)
+    const post = await getPostByPostUrlAndUserId(postUrl, id)
+    const result = {
+      id: post.rows[0].id,
+      postUrl: post.rows[0].postUrl,
+      userId: post.rows[0].userId,
     }
+    return res.status(201).send(result)
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+}
+
+export async function likePost(req, res) {
+  const { id } = req.params
+
+  try {
+    await dbLikePost(id, userId) //ainda nao tem acesso ao userID(fazer middlewara authValidation para pegar)
+    res.send("Post liked")
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+export async function dislikePost(req, res) {
+  const { id } = req.params
+
+  try {
+    await dbDislikePost(id, userId) //ainda nao tem acesso ao userID(fazer middlewara authValidation para pegar)
+    res.send("Post disliked")
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 }
--- src/middlewares/posts.middlewares.js
+++ src/middlewares/posts.middlewares.js
@@ -1,30 +1,44 @@
-import fetch from "node-fetch";
-import { getUserById } from "../repository/users.repositories.js";
-export async function checkIfUrlIsAvailable (req, res, next){
-  const {postUrl} = req.body;
+import fetch from "node-fetch"
+import { getUserById } from "../repository/users.repositories.js"
+import { dbGetPostById } from "../repository/posts.repositories.js"
+export async function checkIfUrlIsAvailable(req, res, next) {
+  const { postUrl } = req.body
 
-  try{
-      const response = await fetch(postUrl);
+  try {
+    const response = await fetch(postUrl)
 
-      if (response.ok) {
-          next();
-        } else {
-          return res.status(404).send('URL not found!');
-        }
-  } catch (err){
-      return res.status(500).send(err.message);
+    if (response.ok) {
+      next()
+    } else {
+      return res.status(404).send("URL not found!")
+    }
+  } catch (err) {
+    return res.status(500).send(err.message)
   }
 }
 
-export async function checkExistingUser (req, res, next){
-  const {id} = req.params;
+export async function checkExistingUser(req, res, next) {
+  const { id } = req.params
 
-  try{
-    const user = await getUserById(id);
-    if (user.rowCount === 0) return res.status(404).send('Usuário not found!');
-    next();
-} catch (err){
-    return res.status(500).send(err.message);
+  try {
+    const user = await getUserById(id)
+    if (user.rowCount === 0) return res.status(404).send("Usuário not found!")
+    next()
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
 }
 
+export async function checkExistingPost(req, res, next) {
+  const { id } = req.params
+
+  if (!id) return res.status(404).send("Post not found!")
+
+  try {
+    const post = await dbGetPostById(id)
+    if (post.rowCount === 0) return res.status(404).send("Post not found!")
+    next()
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
 }
