9d30155f3d77c437e4d71a148c040ed8777eb0a3
Amanda Xavier <amandavxavier@gmail.com>
2023-05-31 21:18:48 -0300

    Feat: posts route GET /posts

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,7 +3,7 @@ import validateRequestBody from "../middlewares/schema.middleware.js"
 import { createPostSchema, updatePostSchema } from "../schemas/posts.schemas.js"
 import {checkExistingPost,checkExistingPostForUpdate,checkExistingUser, checkIfUrlIsAvailable, 
 checkIfUserIsPostsOwner} from "../middlewares/posts.middlewares.js"
-import {createPost, dislikePost, likePost, updateUserPost} from "../controllers/posts.controllers.js"
+import {createPost, dislikePost, getAllUsersPosts, likePost, updateUserPost} from "../controllers/posts.controllers.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
 
 const postsRouter = Router()
@@ -14,6 +14,7 @@ postsRouter.post("/users/:id/posts", validateRequestBody(createPostSchema), chec
 checkExistingUser, createPost);
 postsRouter.put("/users/:userId/posts/postId", checkExistingPostForUpdate, checkIfUserIsPostsOwner, 
 validateRequestBody(updatePostSchema), updateUserPost);
+postsRouter.get("/posts", getAllUsersPosts);
 postsRouter.post("/posts/:id/like", checkExistingPost, likePost);
 postsRouter.post("/posts/:id/dislike", checkExistingPost, dislikePost);
 
