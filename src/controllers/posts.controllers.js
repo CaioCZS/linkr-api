66ba02467745e1cbc29166429ec9765297b30f14
Caio <caiozs21@gmail.com>
2023-06-01 22:59:21 -0300

    feat:  get posts by hashtags

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,63 +1,69 @@
-import {createPostDB, dbDislikePost, dbLikePost, getAllUsersPostsDB, 
-getPostByPostUrlAndUserId, updateUserPostDB} from "../repository/posts.repositories.js";
+import {
+  createPostDB,
+  dbDislikePost,
+  dbGetPostsByHashtag,
+  dbLikePost,
+  getAllUsersPostsDB,
+  getPostByPostUrlAndUserId,
+  updateUserPostDB,
+} from "../repository/posts.repositories.js"
 
 export async function createPost(req, res) {
-  const { id } = req.params;
-  const { description, postUrl } = req.body;
+  const { id } = req.params
+  const { description, postUrl } = req.body
 
   try {
-    await createPostDB(description, postUrl, id);
-    const post = await getPostByPostUrlAndUserId(postUrl, id);
+    await createPostDB(description, postUrl, id)
+    const post = await getPostByPostUrlAndUserId(postUrl, id)
     const result = {
       id: post.rows[0].id,
       postUrl: post.rows[0].postUrl,
       userId: post.rows[0].userId,
-    };
-    return res.status(201).send(result);
+    }
+    return res.status(201).send(result)
   } catch (err) {
-    return res.status(500).send(err.message);
+    return res.status(500).send(err.message)
   }
 }
 
-export async function getAllUsersPosts (req, res) {
-    try {
-        const posts = await getAllUsersPostsDB();
-        return res.send(posts.rows[0]);
-    } catch (err) {
-        return res.status(500).send(err.message);
-    }
-}
-
-export async function updateUserPost (req,res) {
-    const {id} = req.params;
-    const {description} = req.body;
-    const {session} = res.locals;
-    const {userId} = session;
-    try {
-        await updateUserPostDB (description, userId, id);
-        return res.send("Post updated!");
-      } catch (err) {
-        res.status(500).send(err.message)
-      }
-    
-}
-
-export async function deleteUserPost (req,res) {
-  const { id } = req.params;
+export async function getAllUsersPosts(req, res) {
   try {
-    await deletePost(id); 
-    res.send("Post deleted!");
+    const posts = await getAllUsersPostsDB()
+    return res.send(posts.rows[0])
   } catch (err) {
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message)
+  }
+}
+
+export async function updateUserPost(req, res) {
+  const { id } = req.params
+  const { description } = req.body
+  const { session } = res.locals
+  const { userId } = session
+  try {
+    await updateUserPostDB(description, userId, id)
+    return res.send("Post updated!")
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+export async function deleteUserPost(req, res) {
+  const { id } = req.params
+  try {
+    await deletePost(id)
+    res.send("Post deleted!")
+  } catch (err) {
+    res.status(500).send(err.message)
   }
 }
 export async function likePost(req, res) {
   const { id } = req.params
-  const {session} = res.locals
-  const {userId} = session
+  const { session } = res.locals
+  const { userId } = session
   try {
-    await dbLikePost(id, userId) 
-    res.send("Post liked")
+    await dbLikePost(id, userId)
+    res.send({ message: "Post liked" })
   } catch (err) {
     res.status(500).send(err.message)
   }
@@ -65,11 +71,24 @@ export async function likePost(req, res) {
 
 export async function dislikePost(req, res) {
   const { id } = req.params
-  const {session} = res.locals
-  const {userId} = session
+  const { session } = res.locals
+  const { userId } = session
   try {
-    await dbDislikePost(id, userId) 
-    res.send("Post disliked")
+    await dbDislikePost(id, userId)
+    res.send({ message: "Post disliked" })
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+export async function getPostsByHashtag(req, res) {
+  const { hashtag } = req.params
+
+  try {
+    const { rows: result } = await dbGetPostsByHashtag(hashtag)
+    if (result.length === 0)
+      res.status(404).send({ message: "Esta hashtag não existe" })
+    res.send(result)
   } catch (err) {
     res.status(500).send(err.message)
   }
--- src/repository/posts.repositories.js
+++ src/repository/posts.repositories.js
@@ -14,20 +14,23 @@ export function getPostByPostUrlAndUserId(postUrl, userId) {
   )
 }
 
-export function getAllUsersPostsDB(){
-  return db.query(`SELECT * FROM posts ORDER BY "createdAt" DESC LIMIT 20;`);
+export function getAllUsersPostsDB() {
+  return db.query(`SELECT * FROM posts ORDER BY "createdAt" DESC LIMIT 20;`)
 }
 
-export function getUserPostById (postId) {
-  return db.query(`SELECT * FROM posts WHERE id=$1;`, [postId]);
+export function getUserPostById(postId) {
+  return db.query(`SELECT * FROM posts WHERE id=$1;`, [postId])
 }
 
-export function updateUserPostDB (description, userId, postId){
-  return db.query(`UPDATE posts SET description=$1 WHERE id=$2 AND "userId"=$3;`, [description, postId, userId]);
+export function updateUserPostDB(description, userId, postId) {
+  return db.query(
+    `UPDATE posts SET description=$1 WHERE id=$2 AND "userId"=$3;`,
+    [description, postId, userId]
+  )
 }
 
 export function deletePost(postId) {
-  return db.query(`DELETE FROM posts WHERE id=$1;`, [postId]);
+  return db.query(`DELETE FROM posts WHERE id=$1;`, [postId])
 }
 export function dbLikePost(postId, likerId) {
   return db.query(`INSERT INTO likes ("postId","likerId") VALUES ($1,$2)`, [
@@ -41,4 +44,37 @@ export function dbDislikePost(postId, likerId) {
     postId,
     likerId,
   ])
+}
+
+export function dbGetPostsByHashtag(hashtag) {
+  return db.query(
+    `
+  SELECT  posts.*,users.username,users.image,
+	(SELECT JSON_AGG(JSON_BUILD_OBJECT('id' ,h.id,'hashtag',h.name ) 
+		)FROM hashtags h
+	 JOIN posts_hashtags ph ON h.id = ph."hashtagId"
+	 WHERE ph."postId" = posts.id
+	) as hashtags,
+		(SELECT COUNT(likes."postId") 
+        FROM likes 
+        WHERE likes."postId" = posts.id
+    ) as "likesCount",
+	 (SELECT JSON_AGG(JSON_BUILD_OBJECT('id', users.id, 'name', users.username)) 
+        FROM likes
+        JOIN users ON users.id = likes."likerId"
+        WHERE likes."postId" = posts.id
+    ) as likers
+	FROM posts_hashtags
+			JOIN posts ON posts.id = posts_hashtags."postId"
+			JOIN users ON users.id = posts."userId"
+		WHERE posts.id IN (
+    SELECT posts_hashtags."postId"
+    FROM hashtags
+    JOIN posts_hashtags ON posts_hashtags."hashtagId" = hashtags.id
+    WHERE hashtags.name = $1
+)
+GROUP BY posts.id, users.username, users.image;
+  `,
+    [hashtag]
+  )
 }
