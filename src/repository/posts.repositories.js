8c77b1045f01f29dfe7dba31e723933c2f6abdb7
Caio <caiozs21@gmail.com>
2023-06-04 20:14:03 -0300

    feat:get posts by hashtag

--- src/repository/posts.repositories.js
+++ src/repository/posts.repositories.js
@@ -67,31 +67,29 @@ export function dbDislikePost(postId, likerId) {
 export function dbGetPostsByHashtag(hashtag) {
   return db.query(
     `
-  SELECT  posts.*,users.username,users.image,
-	(SELECT JSON_AGG(JSON_BUILD_OBJECT('id' ,h.id,'hashtag',h.name ) 
-		)FROM hashtags h
-	 JOIN posts_hashtags ph ON h.id = ph."hashtagId"
-	 WHERE ph."postId" = posts.id
-	) as hashtags,
-		(SELECT COUNT(likes."postId") 
-        FROM likes 
-        WHERE likes."postId" = posts.id
-    ) as "likesCount",
-	 (SELECT JSON_AGG(JSON_BUILD_OBJECT('id', users.id, 'name', users.username)) 
-        FROM likes
-        JOIN users ON users.id = likes."likerId"
-        WHERE likes."postId" = posts.id
-    ) as likers
-	FROM posts_hashtags
-			JOIN posts ON posts.id = posts_hashtags."postId"
-			JOIN users ON users.id = posts."userId"
-		WHERE posts.id IN (
+    SELECT 
+    p.*, u.username, u.image,
+    (SELECT json_agg(json_build_object('id', h.id, 'hashtag', h.name))
+     FROM hashtags h
+     JOIN posts_hashtags ph ON h.id = ph."hashtagId"
+     WHERE ph."postId" = p.id
+    ) AS hashtags,
+    COUNT(l."postId") AS "likesCount",
+    (SELECT json_agg(json_build_object('id', u.id, 'name', u.username))
+     FROM likes l
+     JOIN users u ON u.id = l."likerId"
+     WHERE l."postId" = p.id
+    ) AS likers
+FROM posts p
+JOIN users u ON u.id = p."userId"
+LEFT JOIN likes l ON l."postId" = p.id
+WHERE p.id IN (
     SELECT posts_hashtags."postId"
     FROM hashtags
     JOIN posts_hashtags ON posts_hashtags."hashtagId" = hashtags.id
     WHERE hashtags.name = $1
 )
-GROUP BY posts.id, users.username, users.image;
+GROUP BY p.id, u.username, u.image;
   `,
     [hashtag]
   )
