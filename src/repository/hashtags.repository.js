12f6fb4c82ce3547d1977dbaca4cc31e2d11a226
Caio <caiozs21@gmail.com>
2023-06-04 23:29:15 -0300

    feat: change query get hashtags

--- src/repository/hashtags.repository.js
+++ src/repository/hashtags.repository.js
@@ -1,6 +1,10 @@
 import db from "../database/db.connection.js"
 
 export function dbGetHashtags() {
-  const hashtags = db.query(`SELECT * FROM hashtags`)
+  const hashtags =
+    db.query(`SELECT COUNT(posts_hashtags."hashtagId") as "timesUsed",posts_hashtags."hashtagId",hashtags.name FROM posts_hashtags
+  JOIN hashtags ON hashtags.id = posts_hashtags."hashtagId"
+  GROUP BY "hashtagId",hashtags.name 
+  ORDER BY "timesUsed" DESC LIMIT 10;`)
   return hashtags
 }
