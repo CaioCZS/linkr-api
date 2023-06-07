19ad2e39e538603d0485a151f59baaa7b3b659b6
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-06-07 18:26:27 -0300

    fix 401

--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -4,12 +4,11 @@ import {
   getFollow,
   postFollow,
 } from "../controllers/follow.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const followRouter = Router();
 
-followRouter.get("/follow", authMiddleware, getFollow);
-followRouter.post("/follow", authMiddleware, postFollow);
-followRouter.delete("/follow", authMiddleware, deleteFollow);
+followRouter.get("/follow", getFollow);
+followRouter.post("/follow", postFollow);
+followRouter.delete("/follow", deleteFollow);
 
 export default followRouter;
