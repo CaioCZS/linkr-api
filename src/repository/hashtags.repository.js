f31acb27f4a5b2b37a3c4cb58c38d3cc5d819c3e
Caio <caiozs21@gmail.com>
2023-06-09 20:47:18 -0300

    feat fianl

--- src/repository/hashtags.repository.js
+++ src/repository/hashtags.repository.js
@@ -5,6 +5,26 @@ export function dbGetHashtags() {
     db.query(`SELECT COUNT(posts_hashtags."hashtagId") as "timesUsed",posts_hashtags."hashtagId",hashtags.name FROM posts_hashtags
   JOIN hashtags ON hashtags.id = posts_hashtags."hashtagId"
   GROUP BY "hashtagId",hashtags.name 
-  ORDER BY "timesUsed" DESC LIMIT 10;`)
+  ORDER BY "timesUsed",posts_hashtags."hashtagId" DESC LIMIT 10;`)
   return hashtags
+}
+
+export function dbGetAllHashtags() {
+  return db.query(`SELECT * FROM hashtags`)
+}
+
+export function dbAddHashtagTable(hashtags, dbHastags) {
+  const values = []
+  hashtags.forEach((h) => {
+    if (!dbHastags.some((hashtag) => hashtag.name === h)) {
+      values.push(h)
+    }
+  })
+
+  const query = `
+  INSERT INTO hashtags (name)
+  VALUES ${values.map((_, index) => `($${index + 1})`).join(", ")}
+`
+
+  return db.query(query, values)
 }
